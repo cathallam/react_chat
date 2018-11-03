813d5d9e97b5a6ea47a7d09b7fc33fe94f97d420
Cat <33282760+cathallam@users.noreply.github.com>
2018-11-03 19:00:30 +0000

    Add chat screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import UsernameForm from './components/UsernameForm';
+import ChatPanel from './components/ChatPanel';
 
+// Render UserNameForm and connect to the onUsernameSumitted event handler 
 class App extends Component {
   constructor() {
     super()
     this.state= {
       currentUsername: '',
+      currentScreen: 'UsernameScreen'
     }
     this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
   }
-
+// WHen onUsernameSubmitted is called, send a POST request to /users route. If reqest is successful, update this.state.username so it's referenced later
   onUsernameSubmitted(username) {
     fetch('http://localhost:3001/users', {
       method: 'POST',
@@ -20,16 +23,22 @@ class App extends Component {
   })
   .then(response => {
     this.setState({
-      currentUsername: username
+      currentUsername: username,
+      currentScreen: 'ChatPanel'
       })
     })
     .catch(error => console.error('Something went wrong', error))
   }
 
   render() {
-    return <h1>Vice Chat</h1>
-    return <UsernameForm onSubmit={this.onUsernameSubmitted} />
+    if (this.state.currentScreen === 'UsernameScreen') {
+      return <UsernameForm onSubmit={this.onUsernameSubmitted} />
+    }
+    if (this.state.currentScreen === 'ChatPanel') {
+      return <ChatPanel currentUsername={this.state.currentUsername} />
+    }
+
       }
     }
     
-    export default App
+    export default App;
