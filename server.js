91214ebf825440b0f6cd782967ca5c5c3c60cb4a
Cat <33282760+cathallam@users.noreply.github.com>
2018-11-03 17:51:23 +0000

    Create chatkit user and instantiate

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
 
-// Create ChatKit User and handle errors
+// Create ChatKit User and instantiate unique chatkit instance using the Instance Locator and Key
 const chatkit = new Chatkit.default({
   instanceLocator: 'v1:us1:a18ca6ab-f32d-4c74-b04d-7ce9d86d1673',
   key: 'd9542246-a62f-4683-87be-382ff913d445:jsed9bL75/2zBw1P1mNMQR9z19P3c6kDEUhG3RTL7Z4=',
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+// Take a username with users route and create a Chatkit user through the chatkit instance
 app.post('/users', (req, res) => {
   const { username } = req.body
   chatkit
@@ -32,7 +33,7 @@ app.post('/users', (req, res) => {
   })
 })
 
-// Authenticate users
+// Authenticate users and server will respond with a token returned by chatkit authenticate if request is valid
 app.post('/authenticate', (req, res) => {
   const authData = chatkit.authenticate({ userId: req.query.user_id })
   res.status(authData.status).send(authData.body)
