bd9d4063c07bce592e99507e663cf825745290d1
Cat <33282760+cathallam@users.noreply.github.com>
2018-11-03 19:45:20 +0000

    Connect to Chatkit instance

--- src/components/ChatPanel.js
+++ src/components/ChatPanel.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit-client'
 
 class ChatPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: {}
+
+    }
+  }
+// Instantiate ChatKit ChatManager with instanceLocator, userID from this.props.currentUsername and a custom TokenProvider that points to /authenticate route
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: 'v1:us1:a18ca6ab-f32d-4c74-b04d-7ce9d86d1673',
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: 'http://localhost:3001/authenticate',
+      }),
+    })
+    //Once ChatManager has been initialised, it calls connect .connect happens asynchronously and a Promise is returned.
+    chatManager
+    .connect()
+    .then(currentUser => {
+      this.setState({ currentUser })
+    })
+    .catch(error => console.error('Something went wrong', error))
+  }
+
+
   render(){
     return (
       <div>
